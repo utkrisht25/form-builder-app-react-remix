9783c9255cd7035e1dbf0b28ec946438e6a89640
umesh <utkrisht@gmail.com>
2025-07-19 12:56:49 +0530

    delete some files releted to deployment

--- server.js
+++
@@ -1,9 +0,0 @@
-const { createRequestHandler } = require("@remix-run/netlify");
-
-// The path to your server build
-const build = require("./build");
-
-exports.handler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
